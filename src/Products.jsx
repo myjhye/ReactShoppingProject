232214f13bb54dffdcd4e4269a12e2218bb670ce
myjhye <t01087633799@gmail.com>
2023-09-01 21:22:20 +0900

    상품 조건별 정렬 (css 미적용)

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,46 +1,30 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getProducts } from "./api/firebase";
 import ProductCard from "./components/ProductCard";
 import Button from "./components/ui/Button";
 
 export default function Products() {
-
-    const {
-        isLoading,
-        error,
-        data: products
-    } = useQuery(['products'], () => getProducts());
-    // useQuery 훅으로 상품 데이터 가져오기
-
-
-    // 상품 정렬 상태 관리
+    const { isLoading, error, data: products } = useQuery(['products'], () => getProducts());
     const [sortedProducts, setSortedProducts] = useState(null);
+    const [selectedCategory, setSelectedCategory] = useState(null);
+    const [selectedGenderCategory, setSelectedGenderCategory] = useState(null);
 
-    // 가격에 따른 상품 정렬
     const handleSortByPrice = (order) => {
-        
         let sorted;
 
-        // 가격 오름차순 정렬 
         if (order === 'asc') {
             sorted = [...products].sort((a, b) => a.price - b.price);
-        
-        // 가격 내림차순 정렬
         } else if (order === 'desc') {
             sorted = [...products].sort((a, b) => b.price - a.price);
-        
-        // 정렬하지 않음
         } else {
             sorted = [...products];
         }
 
-        // 정렬된 상품을 상태로 설정 => 이로써 정렬된 상품 목록이 화면에 표시됨
         setSortedProducts(sorted);
     };
 
     const handleSortByDate = (order) => {
-        
         let sorted;
 
         if (order === 'latest') {
@@ -50,27 +34,86 @@ export default function Products() {
         }
 
         setSortedProducts(sorted);
-    }
+    };
+
+    const handleCategorySelect = (category) => {
+        setSelectedCategory(category === "전체" ? null : category);
+    };
+
+    const handleGenderCategorySelect = (genderCategory) => {
+        setSelectedGenderCategory(genderCategory === "전체" ? null : genderCategory);
+    };
+
+    const mainCategories = ['전체', '원피스', '모자', '상의', '하의'];
+    const genderCategories = ['전체', '여성', '남성', '공용'];
+
+    const filteredProducts =
+        (selectedCategory || selectedGenderCategory)
+            ? (sortedProducts || products).filter(product => {
+                const categoryMatch = !selectedCategory || product.category === selectedCategory;
+                const genderCategoryMatch = !selectedGenderCategory || product.gender === selectedGenderCategory;
+                return categoryMatch && genderCategoryMatch;
+            })
+            : sortedProducts || products;
 
     return (
         <div className="mt-4">
             <div className="mb-4 space-x-2">
-                <Button text={ '가격 낮은 순' } onClick={ () => handleSortByPrice("asc") } />
-                <Button text={ '가격 높은 순' } onClick={ () => handleSortByPrice("desc") } />
-                <Button text={ '최신 순' } onClick={ () => handleSortByDate("latest") } />
-                <Button text={ '오래된 순' } onClick={ () => handleSortByDate("oldest") } />
+                <Button text={'가격 낮은 순'} onClick={() => handleSortByPrice("asc")} />
+                <Button text={'가격 높은 순'} onClick={() => handleSortByPrice("desc")} />
+                <Button text={'최신 순'} onClick={() => handleSortByDate("latest")} />
+                <Button text={'오래된 순'} onClick={() => handleSortByDate("oldest")} />
             </div>
-            { isLoading && <p>Loading..</p> }
-            { error && { error } }
-            <ul className="grid grid-cols-1 md:grid-cols-3 lg-grid-cols-4 gap-4 p-4">
-                { (sortedProducts || products) &&
-                    (sortedProducts || products).map((product) => (
-                        <ProductCard 
-                            key={ product.id }
-                            product={ product }
+
+            <div className="mb-4 space-x-2">
+                {mainCategories.map(category => (
+                    <label key={category} className="inline-flex items-center">
+                        <input
+                            type="radio"
+                            name="category"
+                            value={category}
+                            checked={selectedCategory === category || (selectedCategory === null && category === '전체')}
+                            onChange={() => handleCategorySelect(category)}
+                            className="mr-1"
+                        />
+                        {category}
+                    </label>
+                ))}
+            </div>
+
+            <div className="mb-4 space-x-2">
+                {genderCategories.map(genderCategory => (
+                    <label key={genderCategory} className="inline-flex items-center">
+                        <input
+                            type="radio"
+                            name="genderCategory"
+                            value={genderCategory}
+                            checked={
+                                selectedGenderCategory === genderCategory ||
+                                (selectedGenderCategory === null && genderCategory === '전체')
+                            }
+                            onChange={() => handleGenderCategorySelect(genderCategory)}
+                            className="mr-1"
+                        />
+                        {genderCategory}
+                    </label>
+                ))}
+            </div>
+
+            {isLoading ? (
+                <p>Loading...</p>
+            ) : error ? (
+                <p>{error.message}</p>
+            ) : (
+                <ul className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+                    {filteredProducts.map((product) => (
+                        <ProductCard
+                            key={product.id}
+                            product={product}
                         />
                     ))}
-            </ul>
+                </ul>
+            )}
         </div>
-    )
+    );
 }
