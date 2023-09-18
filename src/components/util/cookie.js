595b42007619c2d52dd0780f4ce8810eed2700a0
myjhye <t01087633799@gmail.com>
2023-09-19 07:27:57 +0900

    feat: 상품 검색 기록 저장을 위한 별도 쿠키 유틸 생성

---
+++ src/components/util/cookie.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+// 쿠키 생성
+export function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    const expires = 'expires=' + date.toUTCString();
+    document.cookie = name + '=' + value + ';' + expires + '; path=/';
+}
+
+// 쿠키 가져옴
+export function getCookie(name) {
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].split('=');
+        if (cookie[0].trim() === name) {
+            return cookie[1];
+        }
+    }
+    return '';
+}
+
+
+// 검색 기록 관리를 위한 커스텀 훅
+export function useSearchHistory() {
+    const [searchHistory, setSearchHistory] = useState([]);
+
+    useEffect(() => {
+        const storedHistory = getCookie('searchHistory');
+        if (storedHistory) {
+            setSearchHistory(storedHistory.split(','));
+        }
+    }, []);
+
+    return { searchHistory, setSearchHistory };
+}
