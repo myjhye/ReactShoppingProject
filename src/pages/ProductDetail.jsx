66ad27add870fb0cbfbe86f7d7696b316e122b13
myjhye <t01087633799@gmail.com>
2023-10-03 14:51:45 +0900

    feat: 비슷한 상품 목록 조회

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuthContext } from "../context/AuthContext";
 import { useLocation } from "react-router-dom";
 import Button from "../components/ui/Button";
-import { addBookmark, addOrUpdateToCart, getBookmarks, getCart, removeBookmark } from "../api/firebase";
+import { addBookmark, addOrUpdateToCart, getBookmarks, getCart, getSilmilarProducts, removeBookmark } from "../api/firebase";
 import { useQuery } from "@tanstack/react-query";
 import Comment from "../components/Comment";
 import { FaRegBookmark, FaBookmark } from 'react-icons/fa';
 import DefaultButton from "../components/ui/DefaultButton";
 import { formatAgo } from "../components/util/date";
+import { getSimilarProducts } from "../api/firebase";
+import ProductCard from "../components/ProductCard";
 
 export default function ProductDetail() {
 
@@ -125,10 +127,48 @@ export default function ProductDetail() {
     
 
 
+
+
+    ///////////////// 비슷한 상품
+
+    const [similarProducts, setSimilarProducts] = useState([]);
+
+    useEffect(() => {
+        getSimilarProducts(product.category, product.gender)
+            .then((results) => {
+                // 현재 페이지 상품 제외
+                const filteredResults = results.filter((similarProducts) => similarProducts.id !== product.id);
+
+                // 최대 4개까지 조회
+                const limitedResults = filteredResults.slice(0, 4);
+                setSimilarProducts(limitedResults);
+
+                console.log(similarProducts);
+            });
+    }, [product]);
+
+
+
+    // "댓글"과 "비슷한 상품" 탭을 선택하기 위한 상태 변수
+  const [selectedTab, setSelectedTab] = useState("댓글");
+
+  // "댓글" 탭 클릭 핸들러
+  const handleCommentTabClick = () => {
+    setSelectedTab("댓글");
+  };
+
+  // "비슷한 상품" 탭 클릭 핸들러
+  const handleSimilarProductsTabClick = () => {
+    setSelectedTab("비슷한 상품");
+  };
+
+    
+
+
     return (
         <>
             <p className="mx-12 mt-4 text-gray-700">{ product.category } / { product.gender } - {formatAgo(product.date, 'ko')} 등록</p>
-            <section className="flex flex-col md:flex-row p-4">
+            <section className="flex flex-col md:flex-row p-4 mt-10">
                 <img className="max-w-xl mx-auto px-4" src={product.image} alt={product.title} />
                 <div className="w-full basis-5/12 flex flex-col p-4">
                     <h2 className="text-3xl font-bold py-2">{ product.title }</h2>
@@ -162,7 +202,60 @@ export default function ProductDetail() {
                     </div>
                 </div>
             </section>
-            <Comment product={product} />
-        </>
-    )
+
+
+      <hr className="mt-20" />
+
+      {/* 탭 선택 버튼 */}
+      <div className="flex space-x-4 mt-20">
+        <button
+          onClick={handleCommentTabClick}
+          className={`${
+            selectedTab === "댓글" ? "bg-blue-500" : "bg-gray-300"
+          } px-4 py-2 rounded-lg text-white`}
+        >
+          댓글
+        </button>
+        <button
+          onClick={handleSimilarProductsTabClick}
+          className={`${
+            selectedTab === "비슷한 상품" ? "bg-blue-500" : "bg-gray-300"
+          } px-4 py-2 rounded-lg text-white`}
+        >
+          비슷한 상품
+        </button>
+      </div>
+
+      {/* 선택한 탭에 따른 콘텐츠 */}
+      {selectedTab === "댓글" && (
+        <Comment
+          product={product}
+        />
+      )}
+
+      {selectedTab === "비슷한 상품" && (
+        <div className="mt-4">
+        {similarProducts.length === 0 ? (
+            <p className="text-gray-500">비슷한 상품이 없습니다.</p>
+        ) : (
+        <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+            {similarProducts.map((similarProduct) => (
+                <ProductCard
+                    key={similarProduct.id}
+                    product={similarProduct}
+                />
+            ))}
+        </ul>
+        )}
+        </div>
+      )}
+
+    <div style={{ height: '400px' }}></div>
+    </>
+  );
 }
+
+
+
+
+
