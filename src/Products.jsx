aa5a568eb29badd7e9a90a34b9e40ad954240a8c
myjhye <t01087633799@gmail.com>
2023-10-29 19:58:15 +0900

    feat: 최근에 본 상품 조회

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { getProducts } from "./api/firebase";
 import ProductCard from "./components/ProductCard";
@@ -6,142 +6,159 @@ import Button from "./components/ui/Button";
 import GenderFilter from "./components/GenderFilter";
 import CategoryFilter from "./components/CategoryFilter";
 
-export default function Products() {
+// 로컬 스토리지에서 최근에 본 상품 목록을 가져오는 함수
+const getRecentlyViewedFromLocalStorage = () => {
+    const recentlyViewedJson = localStorage.getItem("recentlyViewed");
+    return recentlyViewedJson ? JSON.parse(recentlyViewedJson) : [];
+};
 
-    // 상품 데이터 가져오기
+// 로컬 스토리지에 최근에 본 상품 목록을 저장하는 함수
+const setRecentlyViewedToLocalStorage = (recentlyViewed) => {
+    localStorage.setItem("recentlyViewed", JSON.stringify(recentlyViewed));
+};
+
+export default function Products() {
+    // usequery로 상품 데이터 가져오기
     const { isLoading, error, data: products } = useQuery(['products'], () => getProducts());
-    
-    // 가격, 날짜에 따라 정렬된 상품 목록
+
+    // 가격, 날짜에 따른 상품 목록 정렬
     const [sortedProducts, setSortedProducts] = useState(null);
-    
-    // 선택된 상품 카테고리와 성별 카테고리
+
+    // 선택된 상품 카테고리
     const [selectedCategory, setSelectedCategory] = useState(null);
+
+    // 선택된 성별
     const [selectedGenderCategory, setSelectedGenderCategory] = useState(null);
 
+    // 최근 본 상품 목록
+    const [recentlyViewed, setRecentlyViewed] = useState(getRecentlyViewedFromLocalStorage());
 
+    useEffect(() => {
+        // 로컬 스토리지에 최근에 본 상품 목록 저장
+        setRecentlyViewedToLocalStorage(recentlyViewed);
+    }, [recentlyViewed]);
 
+    // 가격에 따른 상품 정렬 처리
+    const handleSortByPrice = (order) => {
+        let sorted;
 
-    // 가격 및 날짜에 따른 상품 정렬
-    const handleSort = (field, order) => {
+        if (order === 'asc') {
+            sorted = [...products].sort((a, b) => a.price - b.price);
+        } else if (order === 'desc') {
+            sorted = [...products].sort((a, b) => b.price - a.price);
+        } else {
+            sorted = [...products];
+        }
 
-        // 상품 배열 복제 -> 원본 데이터 유지하려고
-        const sorted = [...products]
- 
-        
-        // 복제한 상품을 a, b를 가지고 정렬
-        sorted.sort((a, b) => {
- 
-
-            // 가격
-            if (field === 'price') {
-                // 오름차순: a - b & 내림차순: b - a
-                return order === 'asc' ? a.price - b.price : b.price - a.price;
-            
-
-            // 날짜
-            } else if (field === 'date') {
-                return order === 'oldest' ? new Date(a.date) - new Date(b.date) : new Date(b.date) - new Date(a.date);
-            }
-        });
-
-
-        // 정렬된 결과를 sortedProducts에 저장
         setSortedProducts(sorted);
     };
 
+    // 날짜에 따른 상품 정렬 처리
+    const handleSortByDate = (order) => {
+        let sorted;
 
+        if (order === 'latest') {
+            sorted = [...products].sort((a, b) => new Date(b.date) - new Date(a.date));
+        } else if (order === 'oldest') {
+            sorted = [...products].sort((a, b) => new Date(a.date) - new Date(b.date));
+        }
 
+        setSortedProducts(sorted);
+    };
 
-    // 카테고리 선택 처리
+    // 상품 카테고리를 선택할 때 처리하는 함수 (변경 핸들러)
     const handleCategorySelect = (category) => {
-
-        // 선택한 카테고리가 '전체' -> 선택 해제 -> selectedCategory를 null로
-
-        // 그 외 유저가 선택한 실제 카테고리로 설정 -> category -> 원피스, 상의, 하의....
+        // 선택한 카테고리가 '전체'면 선택한 카테고리를 null로 설정하고, 그렇지 않으면 선택한 카테고리를 설정
         setSelectedCategory(category === "전체" ? null : category);
     };
 
-
-
-
-    // 성별 카테고리 선택 처리
+    // 성별 카테고리를 선택할 때 처리하는 함수 (변경 핸들러)
     const handleGenderCategorySelect = (genderCategory) => {
+        // 선택한 카테고리가 '전체'면 선택한 카테고리를 null로 설정하고, 그렇지 않으면 선택한 카테고리를 설정
         setSelectedGenderCategory(genderCategory === "전체" ? null : genderCategory);
     };
 
-
-
-
-    // 상품 카테고리 & 성별 카테고리 목록
+    // 상품 카테고리, 성별 카테고리 목록
     const mainCategories = ['전체', '원피스', '상의', '하의', '모자', '신발', '기타'];
     const genderCategories = ['전체', '여성', '남성', '공용'];
 
 
 
+    // 선택한 [상품 카테고리, 성별 카테고리]에 따라 상품 필터링
+    const filteredProducts =
+
+        // 사용자가 [상품 카테고리]나 [성별 카테고리] 중 하나를 선택 했다면
+        (selectedCategory || selectedGenderCategory)
+
+            // sortedProducts가 있으면 그 배열을 사용하고, 없으면 원본 products 배열 사용
+            // sortedProducts => 선택한 카테고리에 따라 정렬된 결과를 기반으로 추가 필터링(가격, 날짜) 적용
+            ? (sortedProducts || products)
+                .filter((product) => {
+
+                    // 상품 카테고리 필터 조건 : 선택한 카테고리가 없거나 상품 카테고리가 선택한 카테고리와 일치하는 경우
+                    const categoryMatch = !selectedCategory || product.category === selectedCategory;
+
+                    // 성별 카테고리 필터 조건 : 선택한 성별 카테고리가 없거나 성별 카테고리가 선택한 성별 카테고리와 일치하는 경우
+                    const genderCategoryMatch = !selectedGenderCategory || product.gender === selectedGenderCategory;
+
+                    // 선택한 의류 카테고리와 성별 카테고리에 모두 해당되는 상품만 필터링해서 반환
+                    return categoryMatch && genderCategoryMatch;
+                })
+
+            // 선택한 sortedProducts(가격, 날짜 필터링)가 없으면 정렬된 결과나 products(원본 상품 배열)을 필터링 결과로 사용
+            : sortedProducts || products;
 
 
-    // 필터된 상품 목록
+
+
+
+
+    const handleProductClick = (product) => {
+        
+        // 이미 최근에 본 상품 목록에 있는지 확인
+        const isAlreadyViewed = recentlyViewed.some((p) => p.id === product.id);
+
+        // 이미 본 상품이 아니라면 목록에 추가
+        if (!isAlreadyViewed) {
+            // 최근에 본 상품 목록을 최대 5개까지 유지
+            const updatedRecentlyViewed = [product, ...recentlyViewed.slice(0, 4)];
+            // 로컬 스토리지에 업데이트된 목록 저장
+            setRecentlyViewedToLocalStorage(updatedRecentlyViewed);
+            // 상태 업데이트
+            setRecentlyViewed(updatedRecentlyViewed);
+        }
+    };
     
-    // selectedCategory나 selectedGenderCategory 버튼을 하나라도 클릭 함 -> filteredProducts에 할당(필터링을 함) -> 둘 다 클릭 안 하면 필터링 안 함
-    const filteredProducts = (selectedCategory || selectedGenderCategory)
-
-
-        // selectedCategory나 selectedGenderCategory에 하나라도 값 있어서 필터링
-
-        // sortedProduct 없으면 products 배열로 필터링
-        ? (sortedProducts || products).filter((product) => {
-
-
-            // !selectedCategory -> 선택한 카테고리 없음 -> 자동으로 true 됨
-            // product.category === selectedCategory -> 상품 카테고리가 선택한 카테고리와 일치하는지 -> 일치하면 true
-            const categoryMatch = !selectedCategory || product.category === selectedCategory;
-            
-            const genderCategoryMatch = !selectedGenderCategory || product.gender === selectedGenderCategory;
-            
-            
-
-            // 둘 다 true인 경우에 결과 반환
-            return categoryMatch && genderCategoryMatch;
-        })
-
-
-
-        // selectedCategory나 selectedGenderCategory 버튼 둘 다 클릭 안 해서 필터링 안 함
-
-        // sortedProducts 없으면 products 배열 사용
-        : sortedProducts || products;
-
-
 
 
 
 
     return (
         <div className="mt-4">
-            <div className="mb-4 mt-12 space-x-2">
-                {/* 정렬 버튼 */}
-                <Button text={'가격 낮은 순'} onClick={() => handleSort("price", "asc")} />
-                <Button text={'가격 높은 순'} onClick={() => handleSort("price", "desc")} />
-                <Button text={'최신 순'} onClick={() => handleSort("date", "latest")} />
-                <Button text={'오래된 순'} onClick={() => handleSort("date", "oldest")} />
+            <div className="mb-4 space-x-2">
+                {/* Sorting buttons */}
+                <Button text={'가격 낮은 순'} onClick={() => handleSortByPrice("asc")} />
+                <Button text={'가격 높은 순'} onClick={() => handleSortByPrice("desc")} />
+                <Button text={'최신 순'} onClick={() => handleSortByDate("latest")} />
+                <Button text={'오래된 순'} onClick={() => handleSortByDate("oldest")} />
             </div>
 
             <div className="mb-4 flex items-start space-x-6">
-                {/* 상품/성별 카테고리 선택 */}
+                {/* 상품/성별 카테고리 2개 묶기 */}
                 <div>
-                    <CategoryFilter 
+                    <CategoryFilter
                         mainCategories={mainCategories}
                         selectedCategory={selectedCategory}
                         handleCategorySelect={handleCategorySelect}
                     />
-                    <GenderFilter 
+                    <GenderFilter
                         genderCategories={genderCategories}
                         selectedGenderCategory={selectedGenderCategory}
                         handleGenderCategorySelect={handleGenderCategorySelect}
                     />
                 </div>
 
-                {/* 나머지 내용 */}
+                {/* Rest of the content */}
                 <div className="flex-grow w-0">
                     {isLoading ? (
                         <p>Loading...</p>
@@ -159,6 +176,7 @@ export default function Products() {
                                             <ProductCard
                                                 key={product.id}
                                                 product={product}
+                                                handleProductClick={handleProductClick}
                                             />
                                         ))}
                                     </ul>
@@ -167,8 +185,23 @@ export default function Products() {
                         </div>
                     )}
                 </div>
+                <div className="mt-4">
+                    {/* 최근에 본 상품 목록 */}
+                    <h2 className="mb-2">최근에 본 상품</h2>
+                    <ul>
+                        {recentlyViewed.map((product) => (
+                            <li key={product.id}>
+                                <img 
+                                    src={product.image} 
+                                    alt={product.title}
+                                    className="w-24 h-24 object-cover rounded-lg" 
+                                />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
-            {/* 필터된 상품 없을 때 여백 추가 */}
+            {/* 필터된 상품 없을 때 margin 차이 예방 */}
             <div style={{ height: '500px' }}></div>
         </div>
     );
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,59 +2,28 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { formatAgo } from "./util/date";
 import { AiOutlineMessage } from 'react-icons/ai';
-import { TbSquareLetterN } from 'react-icons/tb';
-import moment from "moment";
+import { useQuery } from "@tanstack/react-query";
 
-export default function ProductCard({ product }) {
+export default function ProductCard({ product, handleProductClick }) {
 
     const navigate = useNavigate();
 
-    // 제품 등록일부터 현재 날짜와의 일(day) 차이 계산 -> 1일 미만이면 isOverThreeDays를 true로
-    const isOverThreeDays = moment().diff(moment(product.date), 'days') < 1;
-
-
-
-
     return (
-
-        // 제품 카드 클릭 시 해당 제품 상세 페이지로 이동하는 이벤트 핸들러 설정
         <li
-            onClick={() => { navigate(`/products/${product.id}`, { state: { product } }) }}
+            onClick={() => {
+                handleProductClick(product); // 최근 본 상품 목록 업데이트
+                navigate(`/products/${product.id}`, { state: { product } }); // 페이지 이동
+            }}
             className="rounded-lg shadow-md overflow-hidden cursor-pointer"
         >
-
-            {/* 제품 이미지 */}
             <img className='w-full' src={product.image} alt={product.title} />
-            
-            
-            <div className="mt-2 px-2 text-lg justify-between items-center">
-                <div className="flex">
-
-                    {/* 제품 제목 */}
-                    <h3 className="truncate">{product.title}</h3>
-                        
-                        {/* isOverThreeDays가 true면 N 아이콘 */}
-                        {isOverThreeDays && (
-                            <div class="text-red-500">
-                                <TbSquareLetterN />
-                            </div>
-                        )}   
-                </div>
-
-                <br />
-
-                {/* 제품 가격 */}
-                <p class="text-lg font-bold">{`${product.price.toLocaleString()}원`}</p>
+            <div className="mt-2 px-2 text-lg flex justify-between items-center">
+                <h3 className="truncate">{product.title}</h3>
+                <p>{`₩${product.price}`}</p>
             </div>
-
-            {/* 제품 카테고리 & 성별 */}
             <p className="mb-2 px-2 text-gray-600">{product.category} / {product.gender}</p>
             <div className="mt-2 px-2 text-lg flex justify-between items-center">
-                
-                {/* 제품 등록일 */}
-                <p className="mb-2 px-2 text-gray-600">{formatAgo(product.date, 'ko')}</p>
-                
-                {/* 댓글 수 (추후 구현) */}
+                <p className="mb-2 px-2 text-gray-600">{formatAgo(product.date, 'ko')} 등록</p>
                 <p>
                     <AiOutlineMessage />
                 </p>
