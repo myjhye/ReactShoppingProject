d77ec45a66368ea238d3349fedad593a9da7c10d
myjhye <t01087633799@gmail.com>
2023-10-30 01:26:18 +0900

    docs: 주석

--- src/Products.jsx
+++ src/Products.jsx
@@ -8,19 +8,45 @@ import CategoryFilter from "./components/CategoryFilter";
 import { useNavigate } from "react-router-dom";
 import { BiSolidArrowToTop } from 'react-icons/bi';
 
-// 로컬 스토리지에서 최근에 본 상품 목록을 가져오는 함수
-const getRecentlyViewedFromLocalStorage = () => {
-    const recentlyViewedJson = localStorage.getItem("recentlyViewed");
-    return recentlyViewedJson ? JSON.parse(recentlyViewedJson) : [];
-};
 
-// 로컬 스토리지에 최근에 본 상품 목록을 저장하는 함수
+
+
+
+
+// 로컬 스토리지에 최근에 본 상품 목록 저장
 const setRecentlyViewedToLocalStorage = (recentlyViewed) => {
+
+
+    // 로컬 스토리지에 "recentlyViewed"키로 최근에 본 상품 목록을 json 형식으로 저장
     localStorage.setItem("recentlyViewed", JSON.stringify(recentlyViewed));
 };
 
+
+
+
+
+// 로컬 스토리지에서 최근에 본 상품 목록 가져오기
+const getRecentlyViewedFromLocalStorage = () => {
+
+    
+    // 로컬 스토리지에서 "recentlyViewed" 키로 저장된 데이터 가져옴 
+    const recentlyViewedJson = localStorage.getItem("recentlyViewed");
+    
+    
+    
+    // 가져온 데이터가 있으면 json으로 파싱 -> 없으면 빈 배열 반환
+    return recentlyViewedJson ? JSON.parse(recentlyViewedJson) : [];
+};
+
+
+
+
+
+
+
 export default function Products() {
-    // usequery로 상품 데이터 가져오기
+    
+    // 상품 데이터 가져오기
     const { isLoading, error, data: products } = useQuery(['products'], () => getProducts());
 
     // 가격, 날짜에 따른 상품 목록 정렬
@@ -35,87 +61,168 @@ export default function Products() {
     // 최근 본 상품 목록
     const [recentlyViewed, setRecentlyViewed] = useState(getRecentlyViewedFromLocalStorage());
 
+    // 스크롤 시 화면 최상단 이동하는 버튼 보이기 유무
     const [scrollVisible, setScrollVisible] = useState(false);
 
+
+
     const navigate = useNavigate();
+
+
+
+
     
     
-
-
-    useEffect(() => {
-        // 로컬 스토리지에 최근에 본 상품 목록 저장
-        setRecentlyViewedToLocalStorage(recentlyViewed);
-    }, [recentlyViewed]);
+//===============================================================================================
 
 
 
 
-    // 최상단으로 화면 스크롤
+    // 화면 최상단으로 스크롤 핸들러
     const scrollToTop = () => {
 
         window.scrollTo({ top: 0, behavior: 'smooth' })
     }
 
-    // Function to handle scrolling and update the visibility of the scroll button
+
+
+    // 화면 스크롤 버튼 보이기 핸들러 
     const handleScroll = () => {
+        
         if (window.scrollY > 200) {
-        // Adjust the value (200 in this example) to your desired scroll threshold
-        setScrollVisible(true);
+
+            // 화면 아래로 200px 이상 스크롤 시 스크롤 버튼 표시
+            setScrollVisible(true);
+        
         } else {
-        setScrollVisible(false);
+        
+            // 스크롤이 200px 미만이면 스크롤 버튼 숨김
+            setScrollVisible(false);
         }
     };
 
 
+
+    // 화면 스크롤 버튼 보이기 핸들러 실행 -> handleScroll
     useEffect(() => {
+        
         window.addEventListener("scroll", handleScroll);
+        
         return () => {
             window.removeEventListener("scroll", handleScroll);
         };
+
     }, []);
 
 
 
 
-    // 가격에 따른 상품 정렬 처리
+
+    // 상품 클릭 시 로컬 스토리지에 저장 핸들러
+    const handleProductClick = (product) => {
+        
+        
+        // 이미 최근에 본 상품이 목록에 있는지 확인
+        const isAlreadyViewed = recentlyViewed.some((p) => p.id === product.id);
+
+        
+        
+        // 이미 본 상품이 아니라면 목록에 추가
+        if (!isAlreadyViewed) {
+            
+            // 최근에 본 상품 목록 최대 5개로 설정
+            const updatedRecentlyViewed = [product, ...recentlyViewed.slice(0, 4)];
+            
+            // 로컬 스토리지에 업데이트된 목록 저장
+            setRecentlyViewedToLocalStorage(updatedRecentlyViewed);
+            
+            // 상태 업데이트
+            setRecentlyViewed(updatedRecentlyViewed);
+        }
+    };
+
+
+
+
+    // 상품 클릭시 로컬 스토리지에 저장 실행 -> setRecentlyViewedToLocalStorage
+    useEffect(() => {
+        
+        // 로컬 스토리지에 최근에 본 상품 목록 저장
+        setRecentlyViewedToLocalStorage(recentlyViewed);
+    
+    }, [recentlyViewed]);
+
+
+
+
+
+
+    // 가격에 따른 상품 정렬 핸들러
     const handleSortByPrice = (order) => {
+        
         let sorted;
 
         if (order === 'asc') {
+            
             sorted = [...products].sort((a, b) => a.price - b.price);
+        
         } else if (order === 'desc') {
+            
             sorted = [...products].sort((a, b) => b.price - a.price);
+        
         } else {
+            
             sorted = [...products];
         }
 
         setSortedProducts(sorted);
     };
 
-    // 날짜에 따른 상품 정렬 처리
+
+
+
+
+    // 날짜에 따른 상품 정렬 핸들러
     const handleSortByDate = (order) => {
+        
         let sorted;
 
         if (order === 'latest') {
+        
             sorted = [...products].sort((a, b) => new Date(b.date) - new Date(a.date));
+        
         } else if (order === 'oldest') {
+        
             sorted = [...products].sort((a, b) => new Date(a.date) - new Date(b.date));
+        
         }
 
         setSortedProducts(sorted);
     };
 
-    // 상품 카테고리를 선택할 때 처리하는 함수 (변경 핸들러)
+
+
+
+
+
+    // 상품 카테고리 선택 핸들러
     const handleCategorySelect = (category) => {
-        // 선택한 카테고리가 '전체'면 선택한 카테고리를 null로 설정하고, 그렇지 않으면 선택한 카테고리를 설정
+        
+        // 선택한 카테고리가 '전체' -> 선택한 카테고리를 null로 설정 -> 그렇지 않으면 선택한 카테고리를 설정
         setSelectedCategory(category === "전체" ? null : category);
     };
 
-    // 성별 카테고리를 선택할 때 처리하는 함수 (변경 핸들러)
+
+
+
+    // 성별 카테고리 선택 핸들러
     const handleGenderCategorySelect = (genderCategory) => {
-        // 선택한 카테고리가 '전체'면 선택한 카테고리를 null로 설정하고, 그렇지 않으면 선택한 카테고리를 설정
+        
+        // 선택한 카테고리가 '전체' -> 선택한 카테고리를 null로 설정 -> 그렇지 않으면 선택한 카테고리를 설정
         setSelectedGenderCategory(genderCategory === "전체" ? null : genderCategory);
     };
+
+
 
     // 상품 카테고리, 성별 카테고리 목록
     const mainCategories = ['전체', '원피스', '상의', '하의', '모자', '신발', '기타'];
@@ -123,50 +230,45 @@ export default function Products() {
 
 
 
-    // 선택한 [상품 카테고리, 성별 카테고리]에 따라 상품 필터링
-    const filteredProducts =
-
-        // 사용자가 [상품 카테고리]나 [성별 카테고리] 중 하나를 선택 했다면
-        (selectedCategory || selectedGenderCategory)
-
-            // sortedProducts가 있으면 그 배열을 사용하고, 없으면 원본 products 배열 사용
-            // sortedProducts => 선택한 카테고리에 따라 정렬된 결과를 기반으로 추가 필터링(가격, 날짜) 적용
-            ? (sortedProducts || products)
-                .filter((product) => {
-
-                    // 상품 카테고리 필터 조건 : 선택한 카테고리가 없거나 상품 카테고리가 선택한 카테고리와 일치하는 경우
-                    const categoryMatch = !selectedCategory || product.category === selectedCategory;
-
-                    // 성별 카테고리 필터 조건 : 선택한 성별 카테고리가 없거나 성별 카테고리가 선택한 성별 카테고리와 일치하는 경우
-                    const genderCategoryMatch = !selectedGenderCategory || product.gender === selectedGenderCategory;
-
-                    // 선택한 의류 카테고리와 성별 카테고리에 모두 해당되는 상품만 필터링해서 반환
-                    return categoryMatch && genderCategoryMatch;
-                })
-
-            // 선택한 sortedProducts(가격, 날짜 필터링)가 없으면 정렬된 결과나 products(원본 상품 배열)을 필터링 결과로 사용
-            : sortedProducts || products;
 
 
 
 
 
+    // 필터된 상품 목록
+    
+    // selectedCategory나 selectedGenderCategory 버튼을 하나라도 클릭 함 -> filteredProducts에 할당(필터링을 함) -> 둘 다 클릭 안 하면 필터링 안 함
+    const filteredProducts = (selectedCategory || selectedGenderCategory)
 
-    const handleProductClick = (product) => {
-        
-        // 이미 최근에 본 상품 목록에 있는지 확인
-        const isAlreadyViewed = recentlyViewed.some((p) => p.id === product.id);
 
-        // 이미 본 상품이 아니라면 목록에 추가
-        if (!isAlreadyViewed) {
-            // 최근에 본 상품 목록을 최대 5개까지 유지
-            const updatedRecentlyViewed = [product, ...recentlyViewed.slice(0, 4)];
-            // 로컬 스토리지에 업데이트된 목록 저장
-            setRecentlyViewedToLocalStorage(updatedRecentlyViewed);
-            // 상태 업데이트
-            setRecentlyViewed(updatedRecentlyViewed);
-        }
-    };
+        // selectedCategory나 selectedGenderCategory에 하나라도 값 있어서 필터링
+
+        // sortedProduct 없으면 products 배열로 필터링
+        ? (sortedProducts || products).filter((product) => {
+
+
+            // !selectedCategory -> 선택한 카테고리 없음 -> 자동으로 true 됨
+            
+            // product.category === selectedCategory -> 상품 카테고리가 선택한 카테고리와 일치하는지 -> 일치하면 true
+            const categoryMatch = !selectedCategory || product.category === selectedCategory;
+            
+            const genderCategoryMatch = !selectedGenderCategory || product.gender === selectedGenderCategory;
+            
+            
+
+            // 둘 다 true인 경우에 결과 반환
+            return categoryMatch && genderCategoryMatch;
+        })
+
+
+
+        // selectedCategory나 selectedGenderCategory 버튼 둘 다 클릭 안 해서 필터링 안 함
+
+        // sortedProducts 없으면 products 배열 사용
+        : sortedProducts || products;
+
+
+
 
 
     
