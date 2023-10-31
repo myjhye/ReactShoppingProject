7332aaaaa00df65305b2d4016dee34566224db64
myjhye <t01087633799@gmail.com>
2023-10-31 23:19:31 +0900

    docs: 주석

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -13,19 +13,16 @@ export default function Search({ searchTerm, setSearchTerm, navigate }) {
     // 검색 기록 창 열고 닫기
     const [isSearchHistoryOpen, setIsSearchHistoryOpen] = useState(false);
     
+    // 검색어 입력 필드
     const inputRef = useRef(null);
+
     
+    // 검색 결과 상태
     const { setSearchResults } = useAuthContext();
 
 
+
     
-    // 검색어 변경 핸들러
-    const handleSearchChange = (e) => {
-
-        setSearchTerm(e.target.value);
-    }
-
-
 
     // 엔터 키 눌렀을 때 검색 실행
     const handleKeyPress = (e) => {
@@ -36,7 +33,7 @@ export default function Search({ searchTerm, setSearchTerm, navigate }) {
 
 
 
-    // 검색 창 내부 클릭 => 검색 기록 창 열기
+    // 검색 창 내부 클릭 -> 검색 기록 창 열기
     const handleInputClick = () => {
 
         setIsSearchHistoryOpen(true);
@@ -44,7 +41,7 @@ export default function Search({ searchTerm, setSearchTerm, navigate }) {
 
 
 
-    // 검색 창 외부 클릭 => 검색 기록 창 닫기
+    // 검색 창 외부 클릭 -> 검색 기록 창 닫기
     const handleOutsideClick = (e) => {
 
         if (!inputRef.current || !inputRef.current.contains(e.target)) {
@@ -53,21 +50,29 @@ export default function Search({ searchTerm, setSearchTerm, navigate }) {
         }
     }
 
-    // 쿠키에서 검색 기록 로드 => 검색 기록 쿠키 가져와서 배열에 추가 => 이 함수 없으면 searchItem 쿠키 데이터가 다른 쿠키 데이터로 바뀜
+
+
+
+    // 저장된 검색 기록을 쿠키에서 가져와서 상태에 반영
     const loadSearchHistory = () => {
     
-        // searchHistory 이름의 쿠키 가져옴 
+        
+        // 'searchHistory' 이름의 쿠키 가져오기
         const storedHistory = getCookie("searchItem");
+
 
         if (storedHistory) {
 
-            // 가져온 쿠키('검색어1, 검색어2, 검색어3')를 ,로 분리해서 검색 기록에 추가
+            // 가져온 쿠키('검색어1, 검색어2, 검색어3')를 쉼표(,)로 분리해서 검색 기록 상태에 저장
             setSearchHistory(storedHistory.split(","));
         }
 
     };
 
 
+
+
+    // 저장된 검색 기록 쿠키에서 가져오기 -> 초기 렌더링 시
     useEffect(() => {
 
         loadSearchHistory();
@@ -75,39 +80,59 @@ export default function Search({ searchTerm, setSearchTerm, navigate }) {
 
 
 
-    // 검색 실행
+
+
+
+
+    // 검색 실행 핸들러
     const handleSearch = async () => {
         
+
+
+        // 검색어가 있는 경우만 실행
         if (searchTerm) {
             
-            // 기존 검색어와 동일한 검색어가 있는지 확인
+
+            // 기존 검색 기록에서 현재 검색어의 순서 확인
             const index = searchHistory.indexOf(searchTerm);
             
+
+
+            // 기존 검색어와 동일한 검색어가 있다면 -> 기존 검색 기록에 검색어가 없으면 -1
             if (index !== -1) {
                 
-                // 해당 검색어를 검색 기록에서 제거
+                
                 const updatedHistory = [...searchHistory];
                 
+
+                // 해당 검색어를 검색 기록에서 제거
                 updatedHistory.splice(index, 1);
+
                 setSearchHistory(updatedHistory);
             }
     
 
 
 
-            // 검색 기록 업데이트
-
             // 신규 검색어를 맨 앞에 추가하고 중복 제거
             const updatedHistory = [
                 
                 searchTerm, 
-                ...searchHistory.filter(item => item !== searchTerm)
+                ...searchHistory.filter((item) => item !== searchTerm)
             
             ].slice(0, 5); 
             
+
+
+            // 쿠키에 업데이트 된 검색 기록 저장 -> 유효기간 30일
             setCookie("searchItem", updatedHistory.join(','), 30);
+
+
+
+            // 검색 기록 상태에 업데이트된 검색 기록 저장
             setSearchHistory(updatedHistory);
     
+
             
             
             // 검색 결과를 가져오고 상태에 저장
@@ -116,24 +141,36 @@ export default function Search({ searchTerm, setSearchTerm, navigate }) {
             setSearchResults(results);
         }
     
+
+        // 검색 결과 페이지로 이동
         navigate(`/search/${searchTerm}`);
     
+
+
         // 검색 기록 창 닫기
         setIsSearchHistoryOpen(false);
     }
 
 
 
-    // 검색 기록 삭제
+
+
+    // 검색 기록 삭제 핸들러
     const handleDeleteSearchHistory = (term, e) => {
         
+
+
         // 삭제 버튼 클릭 시에도 기록 창이 닫히지 않음
         e.stopPropagation();
     
-        // 선택한 검색어를 검색 기록에서 제거
+
+
+        // 선택한 검색어를 검색 기록에서 제거 -> searchHistory 배열(검색 기록)에서 term(삭제하는 검색어)과 일치하지 않는 요소만 남김
         const updatedHistory = searchHistory.filter((item) => item !== term);
         setSearchHistory(updatedHistory);
     
+
+
         // 업데이트 된 검색 기록 쿠키에 저장
         setCookie("searchItem", updatedHistory.join(","), 30); 
     }
@@ -145,29 +182,32 @@ export default function Search({ searchTerm, setSearchTerm, navigate }) {
 
         // 입력 창 외부 클릭 이벤트 리스너 등록
         document.addEventListener('click', handleOutsideClick);
+        
         return () => {
             
             // 컴포넌트 언마운트 시 이벤트 리스너 제거
             document.removeEventListener('click', handleOutsideClick);
         }
+
     }, []);
 
 
 
 
-    // 검색 기록 항목 클릭 시 해당 검색어로 검색 실행
-    const handleHistoryClick = async (item) => {
-            
-        setSearchTerm(item);
+    // 검색 기록 항목 클릭 -> 해당 검색어로 검색 실행
+    const handleHistoryClick = async (term) => {
         
-        const results = await searchProductByName(item);
+        
+        setSearchTerm(term);
 
-        // 검색 결과를 상태에 저장
+        
+        // 검색 결과 가져와서 상태에 저장
+        const results = await searchProductByName(term);
         setSearchResults(results);
 
 
         // 검색 결과 페이지로 이동
-        navigate(`/search/${item}`);
+        navigate(`/search/${term}`);
 
         
     };
@@ -181,7 +221,7 @@ export default function Search({ searchTerm, setSearchTerm, navigate }) {
             <input 
               type="text"
               value={searchTerm}
-              onChange={handleSearchChange}
+              onChange={ (e) => setSearchTerm(e.target.value) }
               onKeyPress={handleKeyPress}
               className="flex-grow px-2 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-600 mr-2"
               ref={inputRef}
@@ -192,8 +232,8 @@ export default function Search({ searchTerm, setSearchTerm, navigate }) {
           {isSearchHistoryOpen && (
             <div className="absolute top-full left-0 w-full bg-white z-10 mt-2">
                 <ul className="list-none p-0">
+                    
                     {searchHistory.map((item, index) => (
-                        
                         <li className="py-3 px-4 border-b border-gray-200 relative flex items-center" key={index}>
                             <BsArrowClockwise 
                                 style={{ cursor: "pointer" }}   
@@ -214,6 +254,7 @@ export default function Search({ searchTerm, setSearchTerm, navigate }) {
                             </button>
                         </li>
                     ))}
+
                 </ul>
             </div>
         )}
