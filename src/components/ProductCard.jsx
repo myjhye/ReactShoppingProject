75f01511d64e02963e519aaf87dcc68210315bfd
myjhye <t01087633799@gmail.com>
2023-11-04 22:36:27 +0900

    style: 상품 카드 폰트 css 변경

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -16,9 +16,9 @@ export default function ProductCard({ product, handleProductClick }) {
             className="rounded-lg shadow-md overflow-hidden cursor-pointer"
         >
             <img className='w-full' src={product.image} alt={product.title} />
-            <div className="mt-2 px-2 text-lg flex justify-between items-center">
-                <h3 className="truncate">{product.title}</h3>
-                <p>{`₩${product.price}`}</p>
+            <div className="mt-2 px-2 text-lg justify-between items-center">
+                <h3 className="truncate text-lg">{product.title}</h3>
+                <p className="text-xl font-bold">{`${product.price.toLocaleString()}원`}</p>
             </div>
             <p className="mb-2 px-2 text-gray-600">{product.category} / {product.gender}</p>
             <div className="mt-2 px-2 text-lg flex justify-between items-center">
