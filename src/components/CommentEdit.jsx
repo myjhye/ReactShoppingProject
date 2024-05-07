58562ab7f933c0d1d9c12668d4f39ac515b89487
myjhye <t01087633799@gmail.com>
2024-05-07 23:04:55 +0900

    refine

--- src/components/CommentEdit.jsx
+++ src/components/CommentEdit.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Button from "./ui/Button";
 import { updateComment } from "../api/firebase";
 
+// 댓글 수정 박스
 export default function CommentEdit({ comment, commentList, setCommentList, setEditingCommentId }) {
     
     // 수정 중인 댓글 텍스트
@@ -25,35 +26,27 @@ export default function CommentEdit({ comment, commentList, setCommentList, setE
         await updateComment(comment.id, editedText);
     
         // 댓글 수정 후, 수정된 내용을 댓글 목록에 업데이트
-        try 
-        {
+        try {
             // 댓글 목록 순회하며 수정한 댓글을 찾아서 업데이트
             const updatedCommentList = commentList.map((c) => {
-                
-                if (c.id === comment.id) 
-                {
+                if (c.id === comment.id) {
                     return { 
-                        ...c, 
-
-                        // text를 editedText로 업데이트
+                        ...c,
+                        // text를 editedText로 업데이트 
                         text: editedText 
-                    };
+                    }; 
                 }
-                
                 return c;
             });
-
-            setCommentList(updatedCommentList);
-
-            comment.text = editedText;
-            setEditedText(editedText);
-
+        
+            setCommentList(updatedCommentList); // 업데이트된 댓글 목록을 상태에 저장
         } catch (error) {
-            console.error(error);
+            console.error(error); // 오류가 발생하면 콘솔에 출력
         }
-    
+        
         // 댓글 수정 박스 닫기
         setEditingCommentId(null);
+        
     }
     
 
